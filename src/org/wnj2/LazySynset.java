57df4be13520396b8be68498f52422627348f31a
jkawamoto <jkawamoto@265e8637-6b5b-42ab-84b2-60ab1afdbb36>
2009-09-05 07:33:27 +0000

    Add copyright notices
    git-svn-id: svn+ssh://svn.osdn.jp/svnroot/wnj2/trunk@3 265e8637-6b5b-42ab-84b2-60ab1afdbb36

--- src/org/wnj2/LazySynset.java
+++ src/org/wnj2/LazySynset.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2009 Junpei Kawamoto
+ *
+ * This file is part of Japanese WordNet wrapper for Java (Wnj2).
+ * Wnj2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wnj2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Wnj2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.wnj2;
 
 class LazySynset extends Synset{
